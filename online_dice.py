c4fc30d63fdbee29f1db84ee3a6b811867a194b7
taigamatumoto37 <taiga36tkg@gmail.com>
2026-01-17 11:44:26 +0900

    Update online_dice.py

--- online_dice.py
+++ online_dice.py
@@ -95,7 +95,7 @@ CARD_DB = {
     "ランダムシールド": Card("ランダムシールド", "guard", 45, lambda d: True, "ランダム"), 
     "ランダムシールド": Card("ランダムシールド", "guard", 1.0, lambda d: True, "ランダム"),
     "ランダムシールド": Card("ランダムシールド", "guard", 1.5, lambda d: True, "ランダム"),
-    "ランダムシールド": Card("固有ちょいシールド", "guard", 15, lambda d: True, "15ダメージ防ぐ"), 
+    "固有シールド": Card("固有ちょいシールド", "guard", 15, lambda d: True, "15ダメージ防ぐ"), 
 }
 
 
@@ -435,6 +435,7 @@ with st.sidebar:
         all_cards = list(CARD_DB.keys()); new_deck = all_cards * 2; random.shuffle(new_deck)
         update_db({"hp1": 100, "hp2": 100, "p1_hand": [], "p2_hand": [], "p1_used_innate": [], "p2_used_innate": [], "turn": "P1", "turn_count": 0, "pending_damage": 0, "phase": "ATK", "deck": new_deck})
         st.rerun()
+
 
 
 
